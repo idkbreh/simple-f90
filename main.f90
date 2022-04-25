2cb0996bfa3695f9883fe415b953145e65436bcc
C:\Users\idkbreh <93085924+idkbreh@users.noreply.github.com>
2022-04-25 08:28:36 +0700

    Create main.f90

---
+++ main.f90
@@ -0,0 +1,5 @@
+program main
+ implicit none !announce a variable for program when we compile compiler will know
+
+ integer :: int_number = 90 !<type variable>::<variable> = <value of variable>
+ ! integer = int , real = float , logical = boolean , character = word str
